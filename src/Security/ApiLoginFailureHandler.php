b30d9215e7ef9db7c8b18194e3338460d5b90887
Guillaume <guillaume-valsemey@orange.fr>
2026-02-15 17:37:17 +0100

    Fix login handlers: App\\Security namespace, cache:clear OK

--- src/Security/ApiLoginFailureHandler.php
+++ src/Security/ApiLoginFailureHandler.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 
-class ApiLoginFailureHandler implements AuthenticationFailureHandlerInterface
+final class ApiLoginFailureHandler implements AuthenticationFailureHandlerInterface
 {
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): JsonResponse
     {
         return new JsonResponse([
             'success' => false,
-            'error' => 'Invalid credentials',
+            'error' => 'Bad credentials',
         ], 401);
     }
 }
